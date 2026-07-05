d67bf24885381cfb8a6aaa21b9e7b85812d1fb70
LiZi35 <thelizi35@163.com>
2026-07-05 19:37:38 +0800

    添加cors配置

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config'
 
-if (!process.env.PORT || !process.env.JWT_SECRET || !process.env.EXPRESS_SESSION_SECRET) {
+if (
+    !process.env.PORT ||
+    !process.env.JWT_SECRET ||
+    !process.env.EXPRESS_SESSION_SECRET ||
+    !process.env.CORS_ORIGIN
+) {
     throw new Error('.env缺少配置')
 }
 
 export const PORT = Number(process.env.PORT)
 export const SECRET = process.env.JWT_SECRET
-export const CORS_ORIGIN = 'http://localhost:5173'
+export const CORS_ORIGIN = process.env.CORS_ORIGIN
