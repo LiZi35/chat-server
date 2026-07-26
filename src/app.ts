78b84a849680ac2c06018f683cb4d35d74cdecfb
LiZi35 <thelizi35@163.com>
2026-07-26 14:28:23 +0800

    添加安全头&限流

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
+import { rateLimit } from 'express-rate-limit'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 import cors from 'cors'
-import { PORT, CORS_ORIGIN } from './config/env.js'
+import { PORT, CORS_ORIGIN, RUN_ENV } from './config/env.js'
 import authRouter from './routes/auth.js'
 import { setupSocket } from './socket/index.js'
 
@@ -17,7 +19,26 @@ const io = new Server(server, {
     },
 })
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: '请求过于频繁，请稍后再试',
+    standardHeaders: true,
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+})
+
 app.use(express.json())
+if (RUN_ENV === 'production') {
+    app.use(helmet())
+} else {
+    app.use(
+        helmet({
+            contentSecurityPolicy: false,
+        })
+    )
+}
+app.use(limiter)
 app.use(cors({ origin: CORS_ORIGIN, credentials: true }))
 app.use(cookieParser())
 
